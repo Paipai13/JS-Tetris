40f00dd0b8cc121fc50a4764e4d46e5bf35dc9e3
Paipai13 <43116855+Paipai13@users.noreply.github.com>
2020-03-22 11:49:06 -0500

    Finished Product
    This is the first draft of the finished product. I want to add more like a High Score counter as well as text instructions to tell you how to play

--- tetris.js
+++ tetris.js
@@ -1,15 +1,27 @@
-// Meth Meth Method Tetris Game (24:55)//
+// Meth Meth Method Tetris Game (36:41)//
 
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 
 context.scale(20, 20); 
 
-const matrix =[
-	[0, 0, 0],
-	[1, 1, 1],
-	[0, 1, 0],
-];
+function arenaSweep() {
+	let rowCount = 1;
+	outer: for (let y = arena.length -1; y > 0; --y) {
+		for (let x = 0; x < arena[y].length; ++x) {
+			if (arena[y][x] === 0) {
+				continue outer;
+			}
+		}
+		const row = arena.splice(y, 1)[0].fill(0);
+		arena.unshift(row);
+		++y;
+
+		player.score += rowCount * 10;
+		rowCount *= 2;
+
+	}
+}
 
 function collide(arena, player) {
 	const [m, o] = [player.matrix, player.pos];
@@ -24,7 +36,51 @@ function collide(arena, player) {
 	}
 	return false;
 }
-
+function createPiece(type) {
+	if (type === 'T') {
+		return [
+			[0, 0, 0],
+			[1, 1, 1],
+			[0, 1, 0],
+		];
+	} else if (type === 'O') {
+		return [
+			[2, 2],
+			[2, 2],
+		];
+	} else if (type === 'L') {
+		return [
+			[0, 3, 0],
+			[0, 3, 0],
+			[0, 3, 3],
+		];
+	} else if (type === 'J') {
+		return [
+			[0, 4, 0],
+			[0, 4, 0],
+			[4, 4, 0],
+		];
+	} else if (type === 'I') {
+		return [
+			[0, 5, 0, 0],
+			[0, 5, 0, 0],
+			[0, 5, 0, 0],
+			[0, 5, 0, 0],
+		];
+	} else if (type === 'S') {
+		return [
+			[0, 6, 6],
+			[6, 6, 0],
+			[0, 0, 0],
+		];
+	} else if (type === 'Z') {
+		return [
+			[7, 7, 0],
+			[0, 7, 7],
+			[0, 0, 0],
+		];
+	}
+}
 function createMatrix(w, h) {
 	const matrix = [];
 	while (h--) {
@@ -45,7 +101,7 @@ function drawMatrix(matrix, offset){
 matrix.forEach((row, y) => {
 	row.forEach((value, x) => {
 		if (value !== 0) {
-			context.fillStyle = 'red';
+			context.fillStyle = colors[value];
 			context.fillRect(x + offset.x, 
 							 y + offset.y,
 				 			 1, 1,);
@@ -70,7 +126,9 @@ function playerDrop() {
 	if (collide(arena, player)) {
 		player.pos.y--;
 		merge(arena, player);
-		player.pos.y = 0;
+		playerReset();
+		arenaSweep();
+		updateScore ();
 	}
 	dropCounter = 0;
 }
@@ -82,11 +140,34 @@ function playerMove(dir){
 	}
 }
 
+function playerReset(){ 
+	const pieces = 'TJLOSZI';
+	player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+	player.pos.y = 0;
+	player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0);
+	if (collide(arena, player)) {
+		arena.forEach(row =>row.fill(0));
+		player.score = 0;
+		updateScore();
+	}
+}
+
 function playerRotate(dir) {
+	const pos = player.pos.x; 
+	let offset = 1;
 	rotate(player.matrix, dir);
+	while (collide(arena, player)) {
+		player.pos.x += offset;
+		offset = -(offset + (offset > 0 ? 1 : -1));
+		if (offset > player.matrix[0].length) {
+			rotate(player.matrix, -dir);
+			player.pos.x = pos; 
+			return;
+		}
+	}
 }
 function rotate (matrix, dir) {
-	for (let y=0; y < matrix.lenght; ++y) { 
+	for (let y=0; y < matrix.length; ++y) { 
 		for (let x = 0; x < y; ++x) {
 			[
 				matrix[x][y],
@@ -123,12 +204,28 @@ function update(time = 0){
 
 }
 
+
+function updateScore(){
+	document.getElementById('score').innerText = player.score;
+}
 const arena = createMatrix(12, 20);
 
+const colors = [
+	null,
+	'purple',
+	'yellow',
+	'orange',
+	'blue',
+	'aqua',
+	'green',
+	'red',
+];
+
 const player ={
-	pos:{x: 5, y: 5},
-	matrix: matrix,
-};
+	pos:{x: 0, y: 0},
+	matrix: null,
+	score: 0,
+}
 
 document.addEventListener('keydown', event => {
 	if (event.keyCode === 37) {
@@ -145,4 +242,7 @@ document.addEventListener('keydown', event => {
 		playerRotate(1);
 	}
 });
+
+playerReset();
+updateScore();
 update();
